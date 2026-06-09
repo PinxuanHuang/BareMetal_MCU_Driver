d903a48db2b5af496cf2737cb9c65274c270ab0f
PinxuanHuang <hatedog77777@gmail.com>
2026-06-10 01:32:36 +0800

    feat : add memory base address MACRO

---
+++ Drivers/drivers/inc/stm32f407xx.h
@@ -0,0 +1,11 @@
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+/* base address of flash and SRAM memories */
+#define FLASH_BASEADDR						0x08000000U
+#define SRAM1_BASEADDR						0x20000000U
+#define SRAM2_BASEADDR						0x2001C000U
+#define ROM_BASEADDR						0x1FFF0000U
+#define SRAM 								SRAM1_BASEADDR
+
+#endif
