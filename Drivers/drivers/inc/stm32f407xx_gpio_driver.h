af8cf1d16fa0d780a3404dcfd245adaf40982d13
PinxuanHuang <hatedog77777@gmail.com>
2026-06-11 02:32:57 +0800

    feat: add gpio handle & config struct

--- Drivers/drivers/inc/stm32f407xx_gpio_driver.h
+++ Drivers/drivers/inc/stm32f407xx_gpio_driver.h
@@ -3,4 +3,21 @@
 
 #include "stm32f407xx.h"
 
+/* Configuration structure for GPIO pin */
+typedef struct {
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinAltFunMode;
+} GPIO_PinConfig_t;
+
+/* Handle structure for GPIO pin */
+typedef struct {
+	GPIO_RegDef_t *pGPIOBaseAddr;      /* Address of GPIO pin */
+	GPIO_PinConfig_t GPIO_PinConfig;   /* Hold GPIO pin configuration Settings */
+} GPIO_Handle_t;
+
+
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
