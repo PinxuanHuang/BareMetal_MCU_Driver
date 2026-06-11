30e757239f85e3cd16e8d4d4c088312b97135f93
PinxuanHuang <hatedog77777@gmail.com>
2026-06-11 16:16:31 +0800

    feat: add GPIO init and reset function

--- Drivers/drivers/src/stm32f407xx_gpio_driver.c
+++ Drivers/drivers/src/stm32f407xx_gpio_driver.c
@@ -1,1 +1,183 @@
 #include "stm32f407xx_gpio_driver.h"
+
+/* GPIO Clock Enable/Disable */
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_EN();
+		}else if (pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_EN();
+		}else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_EN();
+		}else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_EN();
+		}else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_EN();
+		}else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_EN();
+		}else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_EN();
+		}else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_EN();
+		}else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_EN();
+		}
+	}
+	else
+	{
+		if(pGPIOx == GPIOA)
+		{
+			GPIOA_PCLK_DI();
+		}else if (pGPIOx == GPIOB)
+		{
+			GPIOB_PCLK_DI();
+		}else if (pGPIOx == GPIOC)
+		{
+			GPIOC_PCLK_DI();
+		}else if (pGPIOx == GPIOD)
+		{
+			GPIOD_PCLK_DI();
+		}else if (pGPIOx == GPIOE)
+		{
+			GPIOE_PCLK_DI();
+		}else if (pGPIOx == GPIOF)
+		{
+			GPIOF_PCLK_DI();
+		}else if (pGPIOx == GPIOG)
+		{
+			GPIOG_PCLK_DI();
+		}else if (pGPIOx == GPIOH)
+		{
+			GPIOH_PCLK_DI();
+		}else if (pGPIOx == GPIOI)
+		{
+			GPIOI_PCLK_DI();
+		}
+	}
+}
+
+/* Init GPIO pin */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	 uint32_t temp=0;
+
+	 /* enable the peripheral clock */
+
+	 GPIO_PeriClockControl(pGPIOHandle->pGPIOx, ENABLE);
+
+	/* configure the mode of gpio pin */
+
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG)
+	{
+		/* the non interrupt mode */
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber ) );
+		pGPIOHandle->pGPIOx->MODER &= ~( 0x3 << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER |= temp;
+
+	}else
+	{
+		if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode ==GPIO_MODE_IT_FT )
+		{
+			/* configure the FTSR */
+			EXTI->FTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+			/* Clear the corresponding RTSR bit */
+			EXTI->RTSR &= ~( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+
+		}else if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode ==GPIO_MODE_IT_RT )
+		{
+			/* configure the RTSR */
+			EXTI->RTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+			/* Clear the corresponding RTSR bit */
+			EXTI->FTSR &= ~( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+
+		}else if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_IT_RFT )
+		{
+			/* configure both FTSR and RTSR */
+			EXTI->RTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+			/* Clear the corresponding RTSR bit */
+			EXTI->FTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+		}
+
+		/* configure the GPIO port selection in SYSCFG_EXTICR */
+		uint8_t temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 4 ;
+		uint8_t temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 4;
+		uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+		SYSCFG_PCLK_EN();
+		SYSCFG->EXTICR[temp1] = portcode << ( temp2 * 4);
+
+		/* enable the exti interrupt delivery using IMR */
+		EXTI->IMR |= 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
+	}
+
+	/* configure the speed */
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << ( 2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber) );
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~( 0x3 << ( 2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber)); //clearing
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;
+
+	/* configure the pupd settings */
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << ( 2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber) );
+	pGPIOHandle->pGPIOx->PUPDR &= ~( 0x3 << ( 2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber)); //clearing
+	pGPIOHandle->pGPIOx->PUPDR |= temp;
+
+
+	/* configure the optype */
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber );
+	pGPIOHandle->pGPIOx->OTYPER &= ~( 0x1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber); //clearing
+	pGPIOHandle->pGPIOx->OTYPER |= temp;
+
+	/* configure the alt functionality */
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		/* configure the alt function registers. */
+		uint8_t temp1, temp2;
+
+		temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 8;
+		temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber  % 8;
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0xF << ( 4 * temp2 ) ); //clearing
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << ( 4 * temp2 ) );
+	}
+}
+
+/* Reset GPIO */
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+	if(pGPIOx == GPIOA)
+	{
+		GPIOA_REG_RESET();
+	}else if (pGPIOx == GPIOB)
+	{
+		GPIOB_REG_RESET();
+	}else if (pGPIOx == GPIOC)
+	{
+		GPIOC_REG_RESET();
+	}else if (pGPIOx == GPIOD)
+	{
+		GPIOD_REG_RESET();
+	}else if (pGPIOx == GPIOE)
+	{
+		GPIOE_REG_RESET();
+	}else if (pGPIOx == GPIOF)
+	{
+		GPIOF_REG_RESET();
+	}else if (pGPIOx == GPIOG)
+	{
+		GPIOG_REG_RESET();
+	}else if (pGPIOx == GPIOH)
+	{
+		GPIOH_REG_RESET();
+	}else if (pGPIOx == GPIOI)
+	{
+		GPIOI_REG_RESET();
+	}
+}
