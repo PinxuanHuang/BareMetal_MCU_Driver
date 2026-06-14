f854b5895a5264363051298b1ce502097d6e8735
PinxuanHuang <hatedog77777@gmail.com>
2026-06-14 18:08:10 +0800

    fix: fix compilation error

--- Drivers/drivers/inc/stm32f407xx_gpio_driver.h
+++ Drivers/drivers/inc/stm32f407xx_gpio_driver.h
@@ -72,8 +72,8 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value);
-void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint16_t Value);
-void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
 
 /* IRQ Configuration & ISR handling */
 void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
