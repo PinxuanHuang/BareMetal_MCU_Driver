f7698e2034e29a3d2ad174fdc9e4c8bee1331dc2
PinxuanHuang <hatedog77777@gmail.com>
2026-08-01 15:42:13 +0800

    [Update] Add SPI Control Register config position Macro and Implement SPI_Init

--- Drivers/drivers/src/stm32f407xx_spi_driver.c
+++ Drivers/drivers/src/stm32f407xx_spi_driver.c
@@ -34,3 +34,47 @@ void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
         }
     }
 }
+
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+    /* configure SPI_CR1 register */
+    uint32_t tempreg = 0;
+
+    /* configure device mode */
+    tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << 2;
+
+    /* configure the bus config */
+    if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+    {
+        /* bidi mode should be cleared */
+        tempreg &= ~(1 << 15);
+    }
+    else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+    {
+        /* bidi mode should be set */
+        tempreg |= (1 << 15);
+    }
+    else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+    {
+        /* bidi mode should be cleared */
+        tempreg &= ~(1 << 15);
+        /* RCONLY bit should be set */
+        tempreg |= (1 << 10);
+    }
+    /* Configure SPI serial clock speed */
+    tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR;
+
+    /* Configure DFF */
+    tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF;
+
+    /* configure CPOL */
+    tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
+
+    /* configure CPHA */
+    tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+
+    tempreg |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
+
+    pSPIHandle->pSPIx = tempreg;
+}
+void SPI_DeInit(SPI_RegDef_t *pSPIx) {}
