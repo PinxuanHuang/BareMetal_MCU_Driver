2b4c8222235e3a07391959f8c6b022fbe2aeb4f4
PinxuanHuang <hatedog77777@gmail.com>
2026-08-04 01:25:21 +0800

    [Update] Implement I2C_SendData

--- Drivers/drivers/src/stm32f407xx_i2c_driver.c
+++ Drivers/drivers/src/stm32f407xx_i2c_driver.c
@@ -1,5 +1,70 @@
 #include "stm32f407xx_i2c_driver.h"
 
+static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
+void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
+static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
+static void I2C_ExecuteAddressPhaseRead(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
+static void I2C_ClearADDRFlag(I2C_Handle_t *pI2CHandle);
+
+static void I2C_MasterHandleRXNEInterrupt(I2C_Handle_t *pI2CHandle);
+static void I2C_MasterHandleTXEInterrupt(I2C_Handle_t *pI2CHandle);
+
+static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr)
+{
+    /* 7bit */
+    SlaveAddr = SlaveAddr << 1;
+
+    /* 1 bit as r or w */
+    SlaveAddr &= ~(1);
+
+    /* set into data register */
+    pI2Cx->DR = SlaveAddr;
+}
+
+static void I2C_ClearADDRFlag(I2C_Handle_t *pI2CHandle)
+{
+    uint32_t dummy_read;
+    // check for device mode
+    if (pI2CHandle->pI2Cx->SR2 & (1 << I2C_SR2_MSL))
+    {
+        // device is in master mode
+        if (pI2CHandle->TxRxState == I2C_BUSY_IN_RX)
+        {
+            if (pI2CHandle->RxSize == 1)
+            {
+                // first disable the ack
+                I2C_ManageAcking(pI2CHandle->pI2Cx, DISABLE);
+
+                /* clear the ADDR flag ( read SR1 , read SR2) */
+                dummy_read = pI2CHandle->pI2Cx->SR1;
+                dummy_read = pI2CHandle->pI2Cx->SR2;
+            }
+        }
+        else
+        {
+            /* clear the ADDR flag ( read SR1 , read SR2) */
+            dummy_read = pI2CHandle->pI2Cx->SR1;
+            dummy_read = pI2CHandle->pI2Cx->SR2;
+        }
+    }
+    else
+    {
+        /* device is in slave mode, clear the ADDR flag ( read SR1 , read SR2) */
+        dummy_read = pI2CHandle->pI2Cx->SR1;
+        dummy_read = pI2CHandle->pI2Cx->SR2;
+    }
+}
+
+static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx)
+{
+    pI2Cx->CR1 |= (1 << I2C_CR1_START);
+}
+
+void I2C_GenerateStopCondition(I2C_RegDef_t *pI2Cx)
+{
+    pI2Cx->CR1 |= (1 << I2C_CR1_STOP);
+}
+
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi)
 {
     if (EnOrDi == ENABLE)
@@ -174,4 +239,49 @@ uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName)
         return FLAG_SET;
     }
     return FLAG_RESET;
+}
+
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr)
+{
+    /* I2C start condition */
+    I2C_GenerateStartCondition(pI2CHandle->pI2Cx);
+
+    /* Check SB flag in SR1 */
+    /* Until SB is cleared, SCL will be stretched (pull to low) */
+    while (!I2C_GetFlagStatus(pI2CHandle->pI2Cx, I2C_FLAG_SB))
+    {
+    }
+
+    /* Send Address of slave and rw bit */
+    I2C_ExecuteAddressPhaseWrite(pI2CHandle->pI2Cx, SlaveAddr);
+
+    /* Clear ADDR flag according to its sw sequence */
+    /* Until ADDR is cleared, SCL will be stretched (pull to low) */
+    I2C_ClearADDRFlag(pI2CHandle);
+
+    /* Send data until len becomes 0 */
+    while (Len > 0)
+    {
+        while (!I2C_GetFlagStatus(pI2CHandle->pI2Cx, I2C_FLAG_TXE))
+        {
+        }
+        pI2CHandle->pI2Cx->DR = *pTxbuffer;
+        pTxbuffer++;
+        Len--;
+    }
+
+    /* when Len becomes zero wait for TXE=1 and BTF=1 before generating the STOP condition */
+    /* TXE=1 , BTF=1 , means that both SR and DR are empty and next transmission should begin. When BTF=1, SCL will be stretched (pulled to low) */
+    while (!I2C_GetFlagStatus(pI2CHandle->pI2Cx, I2C_FLAG_TXE))
+    {
+    }
+
+    while (!I2C_GetFlagStatus(pI2CHandle->pI2Cx, I2C_FLAG_BTF))
+    {
+    }
+
+    /* Generate STOP condition and master need not to wait for the completion of stop condition. */
+    /* Generating STOP, automatically clears the BTF */
+    if (Sr == I2C_DISABLE_SR)
+        I2C_GenerateStopCondition(pI2CHandle->pI2Cx);
 }
