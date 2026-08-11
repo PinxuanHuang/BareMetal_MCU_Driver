68afbf6c11777728551b475f19d87bd5e90ac6b4
PinxuanHuang <hatedog77777@gmail.com>
2026-08-12 03:26:17 +0800

    [Update] Fix i2c function naming and move PLL relative functions to rcc driver source file

--- Drivers/drivers/src/stm32f407xx_i2c_driver.c
+++ Drivers/drivers/src/stm32f407xx_i2c_driver.c
@@ -1,7 +1,6 @@
 #include "stm32f407xx_i2c_driver.h"
 
 static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
-void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
 static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
 static void I2C_ExecuteAddressPhaseRead(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
 static void I2C_ClearADDRFlag(I2C_Handle_t *pI2CHandle);
@@ -21,7 +20,7 @@ static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr)
     pI2Cx->DR = SlaveAddr;
 }
 
-static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr)
+static void I2C_ExecuteAddressPhaseRead(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr)
 {
     /* 7bit */
     SlaveAddr = SlaveAddr << 1;
@@ -170,65 +169,6 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
             I2C3_PCLK_DI();
         }
     }
-}
-
-uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
-uint8_t APB1_PreScaler[4] = {2, 4, 8, 16};
-
-uint32_t RCC_GetPLLOutputClock()
-{
-    /* Useless */
-    return 0;
-}
-
-uint32_t RCC_GetPCLK1Value(void)
-{
-    uint32_t pclk1, SystemClk;
-
-    uint8_t clksrc, temp, ahbp, apb1p;
-
-    clksrc = ((RCC->CFGR >> 2) & 0x3);
-
-    if (clksrc == 0)
-    {
-        SystemClk = 16000000;
-    }
-    else if (clksrc == 1)
-    {
-        SystemClk = 8000000;
-    }
-    else if (clksrc == 2)
-    {
-        SystemClk = RCC_GetPLLOutputClock();
-    }
-
-    /* For AHB */
-    temp = ((RCC->CFGR >> 4) & 0xF);
-
-    if (temp < 8)
-    {
-        ahbp = 1;
-    }
-    else
-    {
-        ahbp = AHB_PreScaler[temp - 8];
-    }
-
-    /* For APB1 */
-    temp = ((RCC->CFGR >> 10) & 0x7);
-
-    if (temp < 4)
-    {
-        apb1p = 1;
-    }
-    else
-    {
-        apb1p = APB1_PreScaler[temp - 4];
-    }
-
-    pclk1 = (SystemClk / ahbp) / apb1p;
-
-    return pclk1;
 }
 
 void I2C_Init(I2C_Handle_t *pI2CHandle)
